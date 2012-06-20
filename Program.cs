5d7d3a850cb98aac95e0106226f68100891e4731
Spencer Wood <spencer.wood25@gmail.com>
2012-06-20 19:47:56 -0400

    Made ServerControl class. Will find a better way to organize it.

--- Program.cs
+++ Program.cs
@@ -10,41 +10,8 @@ namespace overlord
 
         static void Main(string[] args)
         {
-            Process mc_server = new Process();
-            try
-            {
-                mc_server.StartInfo.FileName = "startserver.bat";
-                
-                mc_server.StartInfo.CreateNoWindow = false;
-                mc_server.StartInfo.UseShellExecute = false;
-                mc_server.StartInfo.RedirectStandardInput = true;
-                mc_server.StartInfo.RedirectStandardOutput = true;
-                mc_server.Start();
-
-                StreamWriter input_writer = mc_server.StandardInput;
-                
-                String input_text;
-
-                do
-                {
-                    
-                    input_text = Console.ReadLine();
-                    input_writer.WriteLine(input_text);
-                    
-                } while (input_text != "stop");
-
-                input_writer.Close();
-
-                mc_server.WaitForExit();
-                mc_server.Close();
-
-                
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
-                Console.ReadKey();
-            }
+            ServerControl server = new ServerControl();
+            server.console();
 
         }
     }
---
+++ ServerControl.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.ComponentModel;
+
+namespace overlord
+{
+    class ServerControl
+    {
+        public void console()
+        {
+            Process mc_server = new Process();
+            try
+            {
+                mc_server.StartInfo.FileName = "startserver.bat";
+
+                mc_server.StartInfo.CreateNoWindow = false;
+                mc_server.StartInfo.UseShellExecute = false;
+                mc_server.StartInfo.RedirectStandardInput = true;
+                mc_server.StartInfo.RedirectStandardOutput = true;
+                mc_server.Start();
+
+                StreamWriter input_writer = mc_server.StandardInput;
+
+                String input_text;
+
+                do
+                {
+
+                    input_text = Console.ReadLine();
+                    input_writer.WriteLine(input_text);
+
+                } while (input_text != "stop");
+
+                input_writer.Close();
+
+                mc_server.WaitForExit();
+                mc_server.Close();
+
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                Console.ReadKey();
+            }
+        }
+    }
+}
